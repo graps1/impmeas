6d81711431f0938093fad8d0d73c56e82b3f959b
Hans <hans@pop-os.localdomain>
2022-08-23 10:38:16 +0200

    renamed package

--- setup.py
+++ setup.py
@@ -1,17 +1,17 @@
 from setuptools import setup, find_packages
 
 setup(
-        name="importance_measures",
+        name="impmeas",
         version="1.0",
         packages=find_packages(
             include=[
-                "importance_measures",
-                "importance_measures.bdds",
-                "importance_measures.mc"
+                "impmeas",
+                "impmeas.bdds",
+                "impmeas.mc"
             ],
             exclude=[
-                "importance_measures.tests",
-                "importance_measures.notebooks"
+                "impmeas.tests",
+                "impmeas.notebooks"
             ]
         )
 )
