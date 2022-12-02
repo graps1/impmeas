2ffcbd53b293273b409c11e2f79582fc72aa8ec0
graps1 <asdfsolider@googlemail.com>
2022-12-02 09:53:58 +0100

    more tests

--- tests/test_biased_importance.py
+++ tests/test_biased_importance.py
@@ -1,59 +1,75 @@
-import sys; sys.path.append("..")
-
-import binvec
-import binfunc
-import cgm 
-from utils import generate_SRC_parameters, generate_module
-
+import impmeas as imp
 
 TOLERANCE = 1e-10
 
 vs = [ 
-    cgm.omega, 
-    cgm.nu
-]
-
-bz_vs = [ 
-    cgm.get_value("Bz/DCGM"),
-    cgm.get_value("Bz/RCGM")
+    imp.dominating_cgm,
+    imp.rectifying_cgm
 ]
 
 def test_decomposition():
     '''
         check that 
 
-            Bz_x(v_f) = (|~v_f0 & v_f1|/2^{|F - G|})*Bz_x(v_g)
+            Bz_x(v_f) = Bz_g(v_f)*Bz_x(v_g)
 
         whenever f is monotone modular in g, f1 and f0 are the cofactors of f wrt g
 
         hash: hhjfq9K1Fd 
     '''
     for _ in range(100):
-        f,g,f1,f0 = generate_module(3, 3, monotone=True)
+        X = [f"x{i}" for i in range(3)]
+        Y = [f"y{i}" for i in range(3)]
+        f,g,f1,f0 = imp.random_module(X,Y,monotone=True)
+        f_template = imp.Table.var("z").ite(f1,f0)
         for v in vs:
-            coeff = 0
-            for S in binvec.itersets(f.vars - g.vars):
-                coeff += not v(S, f0) and v(S, f1)
-            coeff = coeff/2**(len(f.vars - g.vars))
-            assert abs( cgm.banzhaf(v, "x0", f) - coeff*cgm.banzhaf(v, "x0", g) ) <= TOLERANCE
+            left = imp.banzhaf(v(f), "x0")
+            right = imp.banzhaf(v(g), "x0")*imp.banzhaf(v(f_template), "z")
+            assert abs(left - right ) <= TOLERANCE
 
 
-def test_SRC_weak():
+# def test_SRC_weak():
+#     '''
+#         check that 
+# 
+#             Bz_y(v_f) >= Bz_x(v_f)
+# 
+#         where 
+#             - f is monotonically modular in g and h,
+#             - g = pi(h^S) for a renaming pi and S subset H,
+#             - x in H
+#             - y = pi(x) in G
+#             - g is a substitute for h in f
+# 
+#         hash: ANYgXi6U8c
+#     '''
+#     for _ in range(10):
+#         f,g,f1,f0 = imp.random_module(X,Y,monotone=True)
+#         f_template = imp.Table.var("z").ite(f1,f0)
+#         f,_,_,_,_,x,y = generate_SRC_parameters(2,3,strong=False)
+#         for bz_v in bz_vs:
+#             assert bz_v(y, f) >= bz_v(x, f) - TOLERANCE
+
+def test_SUD():
     '''
         check that 
 
-            Bz_y(v_f) >= Bz_x(v_f)
+            Bz_x(v_f) >= Bz_x(v_h)
 
         where 
-            - f is monotonically modular in g and h,
-            - g = pi(h^S) for a renaming pi and S subset H,
-            - x in H
-            - y = pi(x) in G
-            - g is a substitute for h in f
-
-        hash: ANYgXi6U8c
+            - f, h is monotonically modular in g,
+            - f_{g/1} >= h_{g/1} and f_{g/0} <= h_{g/0}
+            - x in dep(g)
     '''
-    for _ in range(10):
-        f,_,_,_,_,x,y = generate_SRC_parameters(2,3,strong=False)
-        for bz_v in bz_vs:
-            assert bz_v(y, f) >= bz_v(x, f) - TOLERANCE
+    for _ in range(50):
+        X = [f"x{i}" for i in range(3)]
+        Y = [f"y{i}" for i in range(3)]
+        g = imp.random_table(X) 
+        f0 = imp.random_table(Y) 
+        h0 = imp.random_table(Y) | f0  # h0 >= f0
+        h1 = imp.random_table(Y) | h0  # h1 >= h0 <= f1
+        f1 = imp.random_table(Y) | h1  # f1 >= h1
+        h = g.ite(h1,h0)
+        f = g.ite(f1,f0)
+        for v in vs:
+            assert imp.banzhaf(v(f), "x0") >= imp.banzhaf(v(h), "x0") - TOLERANCE
