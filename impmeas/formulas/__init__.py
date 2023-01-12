6e7135ec8a850b90475231285acd0136a0c2efa9
graps1 <asdfsolider@googlemail.com>
2023-01-12 08:15:48 +0100

    renamed formulas -> representation

--- impmeas/formulas/__init__.py
+++
@@ -1,7 +0,0 @@
-from .repr import PseudoBoolFunc
-from .utils import cnf2dimacs, iter_assignments
-from .formula import Formula, set_pmc_solver, get_pmc_solver
-from .gpmc import GPMC
-from .table import Table
-from .buddy import BuddyNode, add_buddy_delete_callback, buddy_initialize
-from .formula_parser import formula2tree
